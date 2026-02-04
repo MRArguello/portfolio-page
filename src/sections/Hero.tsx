93e6a09cc4cd93505ffbc6809602430eb775704f
Rosario Arguello <mrosario.arguello@gmail.com>
2026-02-04 22:21:12 +0100

    update mobile styles

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,14 +10,14 @@ const Hero = forwardRef<HTMLElement>((_, ref) => {
   return (
     <section ref={ref} className="min-h-screen relative flex items-center">
       <HeroBackground />
-      <div className="relative z-10 mx-40 max-w-5xl px-6 pt-48">
-        <h1 className="text-5xl text-primary sm:text-5xl font-semibold tracking-tight">
+      <div className="relative z-10 w-full max-w-7xl mx-auto px-4 md:px-6 pt-24 md:pt-48">
+        <h1 className="text-4xl md:text-5xl text-primary font-semibold tracking-tight">
           {hero.name}
         </h1>
-        <p className="mt-6 text-xl text-neutral-900 dark:text-neutral-400 ">
+        <p className="mt-6 text-lg md:text-xl text-neutral-900 dark:text-neutral-400">
           {hero.intro}
         </p>
-        <p className="mt-6 text-neutral-800 dark:text-neutral-500 max-w-2xl">
+        <p className="mt-6 text-base md:text-lg text-neutral-800 dark:text-neutral-500 max-w-2xl">
           {hero.description}
         </p>
         <SocialLinks className="mt-4" />
